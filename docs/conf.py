28bcd853dd58993d7793bd9c3a7c1ed05c9db3f3
MinoruInachi <minoru.inachi@gmail.com>
2023-07-23 18:46:02 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,8 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
 
+os.environ['MICROPY_VERSION'] = '1.20'
+
 # The members of the html_context dict are available inside topindex.html
 micropy_version = os.getenv('MICROPY_VERSION') or 'latest'
 micropy_all_versions = (os.getenv('MICROPY_ALL_VERSIONS') or 'latest').split(',')
