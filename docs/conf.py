c5cd17de8b3187f6ff283b073bde859ced2f67ca
INACHI Minoru <minoru.inachi@gmail.com>
2023-10-06 22:47:10 +0900

    Enable sphinx_rtf_theme on ReadTheDocs.

--- docs/conf.py
+++ docs/conf.py
@@ -132,7 +132,7 @@ rst_epilog = """
 # on_rtd is whether we are on readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
+if True:  #not on_rtd:  # only import and set the theme if we're building docs locally
     try:
         import sphinx_rtd_theme
         html_theme = 'sphinx_rtd_theme'
